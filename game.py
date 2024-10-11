22a2fd0928126fc446a50a46f236e5ea9118807e
PRATHAMESHATKARE <142227723+prathameshatkare@users.noreply.github.com>
2024-10-11 11:11:26 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import random
 
 def guess_the_number():
-    print("Welcome to Guess the Number Game!")
+    print("Welcome ")
     print("I'm thinking of a number between 1 and 100.")
 
     # The computer selects a random number between 1 and 100
